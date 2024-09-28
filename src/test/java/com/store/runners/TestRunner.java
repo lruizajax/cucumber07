a9797afeca2c959760487cedde1141b9e59b0ca5
Luis Ruiz <lruizajax@gmail.com>
2024-09-28 11:15:54 -0500

    Update TestRunner.java

--- src/test/java/com/store/runners/TestRunner.java
+++ src/test/java/com/store/runners/TestRunner.java
@@ -17,7 +17,7 @@ import io.cucumber.testng.CucumberOptions;
 				"rerun:target/failedrerun.txt",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
-		tags = "@Checkout",
+		tags = "@Login",
 		monochrome = false,
 		dryRun = false,
 		publish = true
